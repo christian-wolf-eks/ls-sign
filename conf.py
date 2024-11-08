7d33a0d27b7fe6adc450560420242dfbef6eba32
Christian Wolf <christian.wolf@eks-intec.de>
2024-11-08 13:48:56 +0100

    Initial commit created from shpinx-quickstart
    Signed-off-by: Christian Wolf <christian.wolf@eks-intec.de>

---
+++ conf.py
@@ -0,0 +1,31 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Test Project'
+copyright = '2024, Christian Wolf and Foo Bar'
+author = 'Christian Wolf and Foo Bar'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.githubpages',
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+#html_theme = 'alabaster'
+#html_theme = 'basic'
+html_theme = 'classic'
+html_static_path = ['_static']
